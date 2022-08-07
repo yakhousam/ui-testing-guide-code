d7c114597a8d6901431424d72ed2e3780b88d1ca
yakhousam <42283612+yakhousam@users.noreply.github.com>
2022-08-07 09:48:53 +0100

    added intersection addon and tests

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,18 +1,22 @@
-const path = require('path');
+const path = require("path");
 
 const toPath = (_path) => path.join(process.cwd(), _path);
 
 module.exports = {
-  staticDirs: ['../public'],
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  staticDirs: ["../public"],
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/preset-create-react-app',
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app",
+    "@storybook/addon-interactions",
   ],
+  features: {
+    interactionsDebugger: true,
+  },
   core: {
     builder: {
-      name: 'webpack5',
+      name: "webpack5",
     },
   },
   webpackFinal: async (config) => {
@@ -22,8 +26,8 @@ module.exports = {
         ...config.resolve,
         alias: {
           ...config.resolve.alias,
-          '@emotion/core': toPath('node_modules/@emotion/react'),
-          'emotion-theming': toPath('node_modules/@emotion/react'),
+          "@emotion/core": toPath("node_modules/@emotion/react"),
+          "emotion-theming": toPath("node_modules/@emotion/react"),
         },
       },
     };
